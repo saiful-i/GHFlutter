2d047bda59926fbd567c4fcdac9561d3fec0bc7a
saiful <mzuquare@gmail.com>
2023-03-20 03:17:55 +0700

    Creating Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,28 @@ class GHFlutterApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GHFlutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(strings.appTitle),
-        ),
-        body: const Center(
-          child: Text(strings.appTitle),
-        ),
-        ),
+    return const MaterialApp(
+      title: strings.appTitle,
+      home: GHFlutter(),
+    );
+  }
+}
+
+class GHFlutter extends StatefulWidget {
+  const GHFlutter({super.key});
+
+  @override
+  State<GHFlutter> createState() => _GHFlutterState();
+}
+
+class _GHFlutterState extends State<GHFlutter> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(strings.appTitle),
+      ),
+      body: const Text(strings.appTitle),
     );
   }
 }
